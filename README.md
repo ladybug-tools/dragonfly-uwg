12124c434f4c6f370958f02028880697f5a2c166
AntoineDao <antoinedao1@gmail.com>
2018-10-17 07:45:43 +0100

    added setup.py and quick readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dragonfly
+
+[![Build Status](https://travis-ci.com/ladybug-tools/dragonfly.svg?branch=master)](https://travis-ci.com/ladybug-tools/dragonfly)
+
+Dragonfly is a utility API to work with UWG.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dragonfly",
+    version="0.2.0",
+    author="ladybug-tools",
+    description="Dragonfly is a utility API to work with UWG",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/dragonfly",
+    dependency_links=[
+        "https://github.com/ladybug-tools/uwg/archive/master.zip"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+    ],
+)
