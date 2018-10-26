cb5103894b8abeebb28b94fcb7d530e01abad066
AntoineDao <antoinedao1@gmail.com>
2018-10-26 09:43:54 +0100

    feat(CI): travis setup

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-import setuptools
+import re
+from setuptools import setup
+import sys
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+with open('dragonfly/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError:
+    pass
+
+setup(
     name="antoine_dragonfly",
-    version="0.3.0",
+    version=version,
     author="ladybug-tools",
     description="Dragonfly is a utility API to work with UWG",
     long_description=long_description,
@@ -16,6 +31,10 @@ setuptools.setup(
     dependency_links=[
         "https://github.com/ladybug-tools/uwg/archive/master.zip"
     ],
+    entry_points='''
+        [console_scripts]
+        semantic-release=semantic_release.cli:main
+    ''',
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "Operating System :: OS Independent",
