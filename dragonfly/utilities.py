7ae478ded513870d05d6cd528f945cca9dd131ba
Chris Mackey <chris@mackeyarchitecture.com>
2018-11-04 00:06:37 -0400

    Fixed stupid bug

--- dragonfly/utilities.py
+++ dragonfly/utilities.py
@@ -2,7 +2,7 @@
 """A collection of auxiliary funtions for checking inputs."""
 
 
-def in_range(self, val, low, high, param_name='parameter'):
+def in_range(val, low, high, param_name='parameter'):
     if val <= high and val >= low:
         return val
     else:
@@ -11,20 +11,7 @@ def in_range(self, val, low, high, param_name='parameter'):
                 param_name, str(low), str(high), str(val)))
 
 
-def length_match(list1, list2, list1_name='list1', list2_name='list2'):
-    if len(list1) == len(list2):
-        return True
-    else:
-        raise ValueError(
-            "Length of {} : {} does not match the length of {} : {}"
-            .format(list1_name,
-                    str(len(list1)),
-                    list2_name,
-                    str(len(list1)))
-                    )
-
-
-def fixRange(self, val, low, high):
+def fixRange(val, low, high):
     if val > high:
         return high
     elif val < low:
