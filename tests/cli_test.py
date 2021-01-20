b12b565e1023396608adb3105b1de83b129282a0
Chris Mackey <chris@ladybug.tools>
2021-01-20 00:52:38 -0500

    fix(deps): Bump version of dragonfly-core

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -5,21 +5,23 @@ from ladybug.config import folders
 from ladybug.futil import nukedir
 
 import os
+import sys
 
 
 def test_simulate_idf():
-    runner = CliRunner()
-    input_model = './tests/json/model_complete_simple.dfjson'
-    input_epw = './tests/epw/singapore.epw'
-    input_sim_par = './tests/json/uwg_sim_par.json'
+    if (sys.version_info >= (3, 0)):
+        runner = CliRunner()
+        input_model = './tests/json/model_complete_simple.dfjson'
+        input_epw = './tests/epw/singapore.epw'
+        input_sim_par = './tests/json/uwg_sim_par.json'
 
-    result = runner.invoke(
-        simulate_model, [input_model, input_epw, '--sim-par-json', input_sim_par])
-    assert result.exit_code == 0
+        result = runner.invoke(
+            simulate_model, [input_model, input_epw, '--sim-par-json', input_sim_par])
+        assert result.exit_code == 0
 
-    epw_dir = os.path.join(folders.default_epw_folder, 'NewDevelopment')
-    output_json = os.path.join(epw_dir, 'NewDevelopment_uwg.json')
-    output_epw = os.path.join(epw_dir, 'NewDevelopment.epw')
-    assert os.path.isfile(output_json)
-    assert os.path.isfile(output_epw)
-    nukedir(epw_dir)
+        epw_dir = os.path.join(folders.default_epw_folder, 'NewDevelopment')
+        output_json = os.path.join(epw_dir, 'NewDevelopment_uwg.json')
+        output_epw = os.path.join(epw_dir, 'NewDevelopment.epw')
+        assert os.path.isfile(output_json)
+        assert os.path.isfile(output_epw)
+        nukedir(epw_dir)
--- tests/run_test.py
+++ tests/run_test.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import os
+import sys
 
 from ladybug.dt import Date
 
@@ -11,13 +12,14 @@ from tests.fixtures.model import two_buildings
 
 def test_run_uwg():
     """Test the running of a model through the UWG."""
-    model = two_buildings()
+    if (sys.version_info >= (3, 0)):
+        model = two_buildings()
 
-    uwg_par = UWGSimulationParameter()
-    uwg_par.run_period.end_date = Date(1, 7)
+        uwg_par = UWGSimulationParameter()
+        uwg_par.run_period.end_date = Date(1, 7)
 
-    epw_path = './tests/epw/boston.epw'
-    uwg_json, urban_epw = run_uwg(model, epw_path, uwg_par)
-    
-    assert os.path.isfile(uwg_json)
-    assert os.path.isfile(urban_epw)
+        epw_path = './tests/epw/boston.epw'
+        uwg_json, urban_epw = run_uwg(model, epw_path, uwg_par)
+        
+        assert os.path.isfile(uwg_json)
+        assert os.path.isfile(urban_epw)
